e96128fdc0b6bc538c2e528e65723ef33bd76d6d
wangkai37 <wangkai37@baidu.com>
2019-01-23 17:46:38 +0800

    bak

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const vueLoader = require('vue-loader');
+
+console.log('vueLoader', vueLoader);
+
+const app = express();
+
+// 设置静态文件
+app.use('/static', express.static('static'));
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(8848, () => {
+    console.log('Example app listening on port 8848!')
+});

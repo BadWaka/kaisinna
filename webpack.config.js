42bf8f82e28822f25394170737ec51084344074e
BadWaka <1456683844@qq.com>
2019-01-31 15:21:56 +0800

    bak

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,20 @@ module.exports = {
                 use: [
                     'vue-loader'
                 ]
+            },
+            // 图片
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            // 字体
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader'
+                ]
             }
         ]
     },

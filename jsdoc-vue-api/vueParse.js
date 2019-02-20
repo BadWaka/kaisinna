e5e42f329d1ae5f28de318937cc6bf6184883f10
BadWaka <1456683844@qq.com>
2019-02-20 19:37:29 +0800

    bak

--- jsdoc-vue-api/vueParse.js
+++ jsdoc-vue-api/vueParse.js
@@ -4,7 +4,12 @@ const path = require('path');
 const vueTemplateCompiler = require('vue-template-compiler');
 const babel = require('@babel/core');
 
+/**
+ * 解析 .vue 文件
+ * @param {string} filePath 要解析的 .vue 文件路径
+ */
 let parseVue = (filePath) => {
+
     // 转换成绝对路径
     filePath = path.resolve(__dirname, filePath);
 
@@ -21,16 +26,24 @@ let parseVue = (filePath) => {
     let sfcObj = vueTemplateCompiler.parseComponent(fileStr);
     // console.log('sfcObj', sfcObj);
 
+    // 用 jsdoc-api 解析，得到 jsdoc 解析结果对象
+    let jsdocObj = jsdocApi.explainSync({
+        source: sfcObj.script.content
+    });
+    console.log('jsdocObj', jsdocObj);
+
     // 用 eval 执行 js 部分，得到 script 对象
     let scriptObj = eval(babel.transformSync(sfcObj.script.content).code);
-    console.log('scriptObj', scriptObj);
+    // console.log('scriptObj', scriptObj);
 
 };
+
+
 
 module.exports = {
     parseVue
 };
 
-let test = '../test/test.vue';
+let test = './test/test.vue';
 parseVue(test);
 

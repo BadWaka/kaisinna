a703503ad438a813926e4a70a6868e3302c85bc1
BadWaka <1456683844@qq.com>
2019-02-22 16:52:08 +0800

    bak

--- jsdoc-vue-api/testJsDocApi.js
+++
@@ -1,8 +0,0 @@
-// https://github.com/jsdoc2md/jsdoc-api
-let jsdoc = require('jsdoc-api');
-
-let result = jsdoc.explainSync({
-    source: '/** example doclet */ \n var example = true'
-});
-
-console.log('result', result);

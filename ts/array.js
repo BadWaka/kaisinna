ede3d800d86cd93c443c4b1f794cdf6146b5b668
BadWaka <1456683844@qq.com>
2019-06-03 20:04:03 +0800

    bak

--- ts/array.js
+++ ts/array.js
@@ -1,1 +1,3 @@
+// let fibonacci: number[] = [1, 1, 2, 3, 5];
+// fibonacci.push('8');
 var fibonacci = [1, 1, 2, 3, 5];
--- ts/array.ts
+++ ts/array.ts
@@ -1,1 +1,9 @@
-let fibonacci: number[] = [1, 1, 2, 3, 5];
+// let fibonacci: number[] = [1, 1, 2, 3, 5];
+// fibonacci.push('8');
+
+// let fibonacci: Array<number> = [1, 1, 2, 3, 5];
+
+interface NumberArray {
+    [index: number]: number;
+}
+let fibonacci: NumberArray = [1, 1, 2, 3, 5];
